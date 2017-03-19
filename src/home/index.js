b13655d6a4231b7b0ec1baad8a1061187d28a369
Ryan Bednar <bednar85@gmail.com>
2017-03-19 15:11:56 -0400

    - change empty arrays to “No Hours Data” - change list markup to a separate function, use a conditional to handle displaying the hours

--- src/home/index.js
+++ src/home/index.js
@@ -47,7 +47,7 @@ class HomePage extends React.Component {
 
     component.getCurrentLocation();
 
-    console.log('component.state: ', component.state);
+    // console.log('component.state: ', component.state);
 
     // console.log('component.state.currentTime: ', component.state.currentTime);
     // console.log('component.state.currentDay: ', component.state.currentDay);
@@ -63,7 +63,7 @@ class HomePage extends React.Component {
 
     component.init();
 
-    console.log('component.state: ', component.state);
+    // console.log('component.state: ', component.state);
   };
 
   // componentDidMount() {
@@ -141,19 +141,32 @@ class HomePage extends React.Component {
     console.log('now: ', now);
     console.log('today: ', today);
     console.log('tomorrow: ', tomorrow);
-
     console.log('component.state.places: ', component.state.places);
+
+    var places = component.state.places.map(function(place, i) {
+      return (
+        <li className="place" key={i}>
+          <h1 className="place__name">{place.name}</h1>
+          <p className="place__address">{place.address}</p>
+          <p className="place__neighborhood">{place.neighborhood}</p>
+          {typeof place.hours === 'string' ? (
+            <div className="place__">
+              <p className="place__">{place.hours}</p>
+            </div>
+          ) : (
+            <div className="place__">
+              <p className="place__">{place.hours[today].day} – {place.hours[today].format_12}</p>
+              <p className="place__">{place.hours[tomorrow].day} – {place.hours[tomorrow].format_12}</p>
+            </div>
+          )}
+        </li>
+      );
+    });
 
     return (
       <Layout className={s.content}>
         <ul>
-          {component.state.places.map((place, i) =>
-            <li key={i}>
-              <h1>{place.name}</h1>
-              <p>{place.address}</p>
-              <p>{place.neighborhood}</p>
-            </li>
-          )}
+          {places}
         </ul>
       </Layout>
     );
